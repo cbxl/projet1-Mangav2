d6e0ce298e212d9a9667e1b63b0b7680d9124aaa
gsaidomar <48364432+gsaidomar@users.noreply.github.com>
2020-10-04 09:57:17 +0200

    carrousel

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidthx').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidthx').removeClass('cS-hidden');
+        } 
+    });  
+  });
